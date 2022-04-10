05d17a2bbb911831e89caa9b4f4ff1f6ff0646c0
Gabriela-Recinos-20003024 <79528844+Gabriela-Recinos-20003024@users.noreply.github.com>
2022-04-10 10:48:54 -0600

    Add files via upload

---
+++ 20003024 API/API/RickAndMortyAPI/app.js
@@ -0,0 +1,39 @@
+let boton = document.querySelector(".btnPersonaje")
+
+function fetchCharacters() {
+    document.querySelector(".Personaje").style.display = 'none'
+    var character1 = document.getElementById("inputPersonaje").value
+    let url = `https://rickandmortyapi.com/api/character/?name=${character1}`
+    console.log(url)
+    console.log(character1)    
+        fetch(url)
+          .then(response => response.json())
+          .then(characters => showCharacters(characters.results));
+}
+
+
+function showCharacters(characters) {
+    document.querySelector(".Personaje").style.display = 'block'
+    let div = document.querySelector(".listaPersonaje")
+    
+    characters.forEach(character => {
+        
+        let personaje = `
+            <li>
+                <img src="${character.image}" alt="" class="listaPersonajes"/> <br />
+                ${character.name} <br />
+                ${character.status} <br />
+                ${character.gender}
+            </li>
+        `
+        // let image = document.createElement('img')
+        // image = character.image
+        // personaje.appendChild(image).
+        //personaje.innerHTML = `Nombre: ${character.name}`
+
+        div.innerHTML += personaje
+    });
+}
+
+
+boton.addEventListener("click", fetchCharacters)
